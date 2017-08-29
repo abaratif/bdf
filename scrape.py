c5abc8bf025d3543f018d52fad28f5ab76a3060b
Arman Baratifar <ab405h@att.com>
2017-08-29 16:36:07 -0500

    Created a Commodity class to better manage code. Set script to fetch data for gold and silver, and to write concatenated results to csv

--- scrape.py
+++ scrape.py
@@ -1,64 +1,73 @@
 import requests
 import pandas as pd
 
-
-GOLD_ID = "8830"
-SILVER_ID = "8836"
-
 START_DATE = "07/29/2017"
 END_DATE = "08/29/2017"
-BASE_URL = "https://www.investing.com/instruments/HistoricalDataAjax"
 
-def scrape(start_date, end_date, commodity_id):
-	"""
-	Given a a date range and a commodity id,
-	scrape historical data from the investing.com site
 
-	Returns a string with the result gotten from the site
-	"""
-	HEADERS = {
-    "X-Requested-With" : "XMLHttpRequest",
-    "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-	}
-
-	data = {
-	    "curr_id" : commodity_id,
-	    "smlID" : "300004",
-	    "st_date" : START_DATE,
-	    "end_date" : END_DATE,
-	    "interval_sec" : "Daily",
-	    "sort_col" : "date",
-	    "sort_ord" : "DESC",
-	    "action": "historical_data"
-	}
-
-	try:
-		
-		r = requests.post(BASE_URL, data=data, headers=HEADERS)
-		assert(r.status_code == 200)
-		return r.text
-
-	except Exception as e:
-		raise e
-
-	return None
-
-def parse(response_text):
-	"""
-	Given a string containing HTML response text
-	parses out the necessary data
+class Commodity(object):
+	"""docstring for Commodity"""
 	
-	returns a pandas DataFrame object  
-	"""
-	df = pd.read_html(response_text)
-	df = df[0]  # Ignore footer table
-	df["Date"] = pd.to_datetime(df["Date"])
-	df["Commodity"] = "Gold"
+	BASE_URL = "https://www.investing.com/instruments/HistoricalDataAjax"
 
-	return df   
+	def __init__(self, start, end, name):
 
-response_text = scrape(START_DATE, END_DATE, commodity_id=GOLD_ID)
-df = parse(response_text)
-df.to_csv()
+		self.start = start
+		self.end = end
+		self.name = name
+		self.response = self.scrape()
 
-print(df)
+	def scrape(self):
+		"""
+		Given a Commodity object with a date range and a commodity id,
+		scrape historical data from the investing.com site
+
+		Stores a string with the result gotten from the site
+		"""
+		HEADERS = {
+	    "X-Requested-With" : "XMLHttpRequest",
+	    "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+		}
+
+		ID_MAPPING = {
+			"gold" : "8830",
+			"silver" : "8836"
+		}
+
+		data = {
+		    "curr_id" : ID_MAPPING[self.name],
+		    "smlID" : "300004",
+		    "st_date" : self.start,
+		    "end_date" : self.end,
+		    "interval_sec" : "Daily",
+		    "sort_col" : "date",
+		    "sort_ord" : "DESC",
+		    "action": "historical_data"
+		}
+
+		try:
+			
+			r = requests.post(self.BASE_URL, data=data, headers=HEADERS)
+			assert(r.status_code == 200)
+			return r.text
+		except Exception as e:
+			raise e
+
+	def to_df(self):
+		"""
+		returns a pandas DataFrame object based on parsed data from a 
+		Commodity object's HTML
+		"""
+		df = pd.read_html(self.response)
+		df = df[0]  # Ignore footer table
+		df["Date"] = pd.to_datetime(df["Date"])
+		df["Commodity"] = self.name
+
+		return df
+	 
+gold = Commodity(START_DATE, END_DATE, "gold").to_df()
+silver = Commodity(START_DATE, END_DATE, "silver").to_df()
+
+combined = gold.append(silver)
+combined.index.name = 'index'
+combined.to_csv("data.csv")
